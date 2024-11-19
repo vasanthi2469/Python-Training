f44ff90eb9945dcb46c30cc922a70614d0c4f1d7
vasanthi2469 <tamalampudivanisanthoshi2469@gmail.com>
2024-11-19 22:25:49 +0530

    Add files via upload

---
+++ Probability.py
@@ -0,0 +1,12 @@
+def Probability_calculation(favorable_outcomes, total_outcomes):
+   
+
+    probability = favorable_outcomes / total_outcomes
+    return probability
+
+total_outcomes = 6
+
+favorable_outcomes = 2
+
+probability = (favorable_outcomes, total_outcomes)
+print("Probability of rolling a number greater than 4:", probability)
